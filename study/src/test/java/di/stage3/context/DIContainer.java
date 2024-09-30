42e5d698fb33cf4f09f6d0d3222c4d1b80891612
Gyeongho Yang <chchch1213@naver.com>
2024-10-01 00:17:43 +0900

    test: solve stage 3

--- study/src/test/java/di/stage3/context/DIContainer.java
+++ study/src/test/java/di/stage3/context/DIContainer.java
@@ -1,6 +1,11 @@
 package di.stage3.context;
 
+import di.ConsumerWrapper;
+import di.FunctionWrapper;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,11 +15,38 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = createBeans(classes);
+        beans.forEach(ConsumerWrapper.accept(this::injectBeansToFields));
+    }
+
+    private Set<Object> createBeans(Set<Class<?>> classes) {
+        return classes.stream()
+                .map(FunctionWrapper.apply(Class::getDeclaredConstructor))
+                .peek(constructor -> constructor.setAccessible(true))
+                .map(FunctionWrapper.apply(Constructor::newInstance))
+                .collect(Collectors.toUnmodifiableSet());
+    }
+
+    private void injectBeansToFields(Object bean) {
+        for (Field field : bean.getClass().getDeclaredFields()) {
+            injectBeansToField(bean, field);
+        }
+    }
+
+    private void injectBeansToField(Object bean, Field field) {
+        field.setAccessible(true);
+        Class<?> fieldType = field.getType();
+        beans.stream()
+                .filter(fieldType::isInstance)
+                .findAny()
+                .ifPresent(ConsumerWrapper.accept(foundBean -> field.set(bean, foundBean)));
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(bean -> bean.getClass().equals(aClass))
+                .findAny()
+                .orElse(null);
     }
 }
