ad18300c85dbabc2d551781ba3a0e7d8e0095088
Gyeongho Yang <chchch1213@naver.com>
2024-10-01 00:41:17 +0900

    test: solve stage 4

--- study/src/test/java/di/stage4/annotations/ClassPathScanner.java
+++ study/src/test/java/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,24 @@
 package di.stage4.annotations;
 
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
+import org.reflections.Reflections;
 
 public class ClassPathScanner {
 
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        Reflections reflections = new Reflections(packageName);
+        Set<Class<?>> services = reflections.getTypesAnnotatedWith(Service.class);
+        Set<Class<?>> repositories = reflections.getTypesAnnotatedWith(Repository.class);
+
+        return CombineSets(services, repositories);
+    }
+
+    @SafeVarargs
+    private static <T> Set<T> CombineSets(Set<T>... sets) {
+        return Arrays.stream(sets)
+                .flatMap(Set::stream)
+                .collect(Collectors.toSet());
     }
 }
